66a1fa415b443d3a4eade020cc74c8f7837b90bf
BotTelegram <95747163+Bottom-T@users.noreply.github.com>
2021-12-23 00:46:10 +0200

    Update start_filters.py

--- root/plugins/start_filters.py
+++ root/plugins/start_filters.py
@@ -19,14 +19,14 @@ import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-@us6a02.on_message(filters.command("help"))
+@CLiENT.on_message(filters.command("help"))
 async def help_user(c,m):
     try:
        await m.reply_text(Translation.HELP_USER,quote=True)
     except Exception as e:
         log.info(str(e))
         
-@us6a02.on_message(filters.command("start"))
+@CLiENT.on_message(filters.command("start"))
 async def start_msg(c,m):
     button = [[
                InlineKeyboardButton("New York", url=f"https://t.me/us7a5}")
@@ -38,7 +38,7 @@ async def start_msg(c,m):
         log.info(str(e))
 
         
-@us6a02.on_message(filters.command("log") & filters.private & filters.user(Config.OWNER_ID))
+@CLiENT.on_message(filters.command("log") & filters.private & filters.user(Config.OWNER_ID))
 async def log_msg(c,m):
   z =await m.reply_text("Processing..", True)
   if os.path.exists("Log.txt"):
