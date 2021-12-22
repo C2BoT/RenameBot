c36bb280e985e7545dd958191109f6d192ce105a
BotTelegram <95747163+Bottom-T@users.noreply.github.com>
2021-12-22 23:34:25 +0200

    Create messages.py

---
+++ root/messages.py
@@ -0,0 +1,9 @@
+class Translation(object):
+    START_TEXT = "**I'm a Rename and Convert Bot\nJust send me any media to change file name.\nUse /help command for more details **"
+######################
+    HELP_USER = """**>>Send File/Video\n>>Select desired Option\n>>And Done wait for it to process files**"""
+    DOWNLOAD_MSG = "**Downloading **⏬"
+    DOWNLOAD_FAIL_MSG = "**Failed to Download File**❎"
+    UPLOAD_MSG = "**Uploading** ⏫"
+    UPLOAD_FAIL_MSG = "**Failed to Upload File**❎"
+    UPLOAD_DONE_MSG = "**Uploaded Successfully 💡"
