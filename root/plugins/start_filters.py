17ee822b4befd31e3ac0c48307f57ae1647dd0a2
BotTelegram <95747163+Bottom-T@users.noreply.github.com>
2021-12-23 01:20:52 +0200

    Update start_filters.py

--- root/plugins/start_filters.py
+++ root/plugins/start_filters.py
@@ -29,7 +29,7 @@ async def help_user(c,m):
 @Client.on_message(filters.command("start"))
 async def start_msg(c,m):
     button = [[
-               InlineKeyboardButton("Owner ", url=f"https://t.me/{Config.OWNER_USERNAME}")
+               InlineKeyboardButton("New York", url=f"https://t.me/us7a5")
              ]]
     markup = InlineKeyboardMarkup(button) 
     try:
