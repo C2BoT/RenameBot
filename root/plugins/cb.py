85b74ad8ce8a7cecb2b8301a2b6524d4dadeab73
BotTelegram <95747163+Bottom-T@users.noreply.github.com>
2022-01-13 01:12:33 +0200

    Update cb.py

--- root/plugins/cb.py
+++ root/plugins/cb.py
@@ -60,8 +60,8 @@ async def renamer(c,m,as_file=False):
   except:
     extension = "mkv"
   await bot_msg.delete() # delete name asked msg 
-  if len(new_f_name) > 1200:
-      await m.reply_text(text=f"𝗟𝗜𝗠𝗜𝗧𝗦 𝗢𝗙 𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 𝗙𝗜𝗟𝗘  𝗡𝗔𝗠𝗘 𝗜𝗦 1200💓 𝗖𝗛𝗔𝗥𝗘𝗖𝗧𝗘𝗥𝗦 𝗢𝗡𝗟𝗬\n𝗥𝗘𝗗𝗨𝗖𝗘 𝗦𝗢𝗠𝗘 𝗔𝗡𝗗 𝗧𝗥𝗬 𝗔𝗚𝗔𝗜𝗡")
+  if len(new_f_name) > 64:
+      await m.reply_text(text=f"𝗟𝗜𝗠𝗜𝗧𝗦 𝗢𝗙 𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 𝗙𝗜𝗟𝗘  𝗡𝗔𝗠𝗘 𝗜𝗦 64 𝗖𝗛𝗔𝗥𝗘𝗖𝗧𝗘𝗥𝗦 𝗢𝗡𝗟𝗬\n𝗥𝗘𝗗𝗨𝗖𝗘 𝗦𝗢𝗠𝗘 𝗔𝗡𝗗 𝗧𝗥𝗬 𝗔𝗚𝗔𝗜𝗡")
       return
   d_msg = await m.reply_text(Translation.DOWNLOAD_MSG,True)
   d_location = Config.DOWNLOAD_LOCATION + "/" + str(m.chat.id) + "/"
