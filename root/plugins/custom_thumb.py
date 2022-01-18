d5fbaf25971ffe5c6d70e9489877fb4234f9af6e
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-01-18 02:46:39 +0200

    Update custom_thumb.py

--- root/plugins/custom_thumb.py
+++ root/plugins/custom_thumb.py
@@ -1,9 +1,9 @@
 '''
 Renam_eBot
 Thanks to Spechide Unkle as always fot the concept  ♥️
-This file is a part of us6a02 rename repo 
+This file is a part of TE_GitHub rename repo 
 Dont kang !!!
-© us6a02
+© TE_GitHub
 '''
 
 import logging
