73a5c9bc59f271d052705ce6cce9820f9a6afbad
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-02-12 18:11:51 +0200

    Update start_filters.py

--- root/plugins/start_filters.py
+++ root/plugins/start_filters.py
@@ -18,7 +18,34 @@ from root.messages import Translation
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+UPDATE_CHANNEL=Config.UPDATE_CHANNEL # Update Channel Forces Subscribe
+BOT_USERNAME=Config.BOT_USERNAME # ReStart Option 
+JOIN=Translation.JOIN_TEXT # Button Text (Update Channel)
+TRY=Translation.TRY_TEXT # Button Text (Update Channel)
+SUB_TEXT=Translation.FSUB_TEXT # FSUB Information Text
 
+@Client.on_message(filters.private & filters.forwarded)
+async def info(motech, msg):
+    update_channel = UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await motech.get_chat_member(update_channel, msg.chat.id)
+            if user.status == "kicked out":
+               await update.reply_text("🔮 Sorry Dude, You are **🅤︎🅢︎🅐︎ 🇺🇸**")
+               return
+        except UserNotParticipant:
+            #await update.reply_text(f"Join @{Channel User Name} To Use Me") From Motech.py
+            await msg.reply_text(
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text=f"{JOIN}", url=f"t.me/{UPDATE_CHANNEL}")],
+                    [ InlineKeyboardButton(text=f"{TRY}", url=f"https://t.me/{BOT_USERNAME}?start=try")]
+              ])
+            )
+            return
+        except Exception:
+            await msg.reply_text(f"@{UPDATE_CHANNEL}")
+            return
+    if msg.forward_from:
 @Client.on_message(filters.command("help"))
 async def help_user(c,m):
     try:
