fc65d519a34690e34fe579646412f4b231e3071d
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-02-12 18:48:32 +0200

    Update Forword.py

--- root/Forword.py
+++ root/Forword.py
@@ -1,1 +1,29 @@
+from pyrogram import filters
+from pyrogram import Client as Client
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
+from Client.Translation import Translation
+from Client.Config import Config
 
+UPDATE_CHANNEL=Config.UPDATE_CHANNEL # Update Channel Forces Subscribe
+BOT_USERNAME=Config.BOT_USERNAME # ReStart Option 
+JOIN=Translation.JOIN_TEXT # Button Text (Update Channel)
+TRY=Translation.TRY_TEXT # Button Text (Update Channel)
+SUB_TEXT=Translation.FSUB_TEXT # FSUB Information Text
+
+@BOTidSBot.on_message(filters.private & filters.forwarded)
+async def info(motech, msg):
+    update_channel = UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await motech.get_chat_member(update_channel, msg.chat.id)
+            if user.status == "kicked out":
+               await update.reply_text("🔮 Sorry Dude, You are **🅤︎🅢︎🅐︎ 🇺🇸**")
+               return
+        except UserNotParticipant:
+            #await update.reply_text(f"Join @{Channel User Name} To Use Me") From Motech.py
+            await msg.reply_text(
+                text=f"<b>{SUB_TEXT}</b>",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text=f"{JOIN}", url=f"t.me/{UPDATE_CHANNEL}")],
+                    [ InlineKeyboardButton(text=f"{TRY}", url=f"https://t.me/{BOT_USERNAME}?start=try")]
