fc8b72348d55fbca94ae6182051766df90365f87
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-02-12 19:50:13 +0200

    Update config.py

--- root/config.py
+++ root/config.py
@@ -10,11 +10,11 @@ import os
 class Config(object):
   APP_ID = int(os.environ.get("APP_ID", ""))
   API_HASH = os.environ.get("API_HASH", "")
-  TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+  TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
   AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
   DOWNLOAD_LOCATION = "./bot/DOWNLOADS"
   DB_URI = os.environ.get("DATABASE_URL", "")
   # owner is for log cmd only owner can use (this can be multiple users)
   OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "").split(" ")]
-  OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "BotDunia")
+  CHANNEL = os.environ.get("CHANNEL", "BotDunia")
   CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION",False)
