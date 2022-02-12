e5608b774fc03fe77c98f7e3e91580b07bf3d8c6
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-02-12 19:36:43 +0200

    Update start_filters.py

--- root/plugins/start_filters.py
+++ root/plugins/start_filters.py
@@ -10,45 +10,6 @@ import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
-
-from pyrogram import Client,filters
-from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
-from root.config import Config
-from root.messages import Translation
-import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-from pyrogram import filters
-from pyrogram import Client as Client
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors import UserNotParticipant
-from Client.Translation import Translation
-from Client.Config import Config
-
-UPDATE_CHANNEL=Config.UPDATE_CHANNEL # Update Channel Forces Subscribe
-BOT_USERNAME=Config.BOT_USERNAME # ReStart Option 
-JOIN=Translation.JOIN_TEXT # Button Text (Update Channel)
-TRY=Translation.TRY_TEXT # Button Text (Update Channel)
-SUB_TEXT=Translation.FSUB_TEXT # FSUB Information Text
-
-@Client.on_message(filters.private & filters.forwarded)
-async def info(motech, msg):
-    update_channel = UPDATE_CHANNEL
-    if update_channel:
-        try:
-            user = await motech.get_chat_member(update_channel, msg.chat.id)
-            if user.status == "kicked out":
-               await update.reply_text("🔮 Sorry Dude, You are **🅤︎🅢︎🅐︎ 🇺🇸**")
-               return
-        except UserNotParticipant:
-            #await update.reply_text(f"Join @{Channel User Name} To Use Me") From Motech.py
-            await msg.reply_text(
-                text=f"<b>{SUB_TEXT}</b>",
-                reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text=f"{JOIN}", url=f"t.me/{UPDATE_CHANNEL}")],
-                    [ InlineKeyboardButton(text=f"{TRY}", url=f"https://t.me/{BOT_USERNAME}?start=try")]
-
-
 @Client.on_message(filters.command("help"))
 async def help_user(c,m):
     try:
