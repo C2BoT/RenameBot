361e58cc5e38bd5f0c892c062d282021669850e1
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-02-24 02:38:17 +0200

    Update custom_thumb.py

--- root/plugins/custom_thumb.py
+++ root/plugins/custom_thumb.py
@@ -27,10 +27,10 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 @Client.on_message(filters.photo)
 async def save_photo(c,m):
-    ok = await m.edit_text("okay",True)
-    if m.from_user.id  is not None:
+    v = await m.reply_text("okay",True)
+    if m.media_group_id is not None:
         # album is sent
-        download_location = Config.DOWNLOAD_LOCATION + "/thumb/" + str(m.from_user.id) + ".jpg"
+        download_location = Config.DOWNLOAD_LOCATION + "/thumb/" + str(m.from_user.id) + ".jpg" + str(m.media_group_id) + "/"
         if not os.path.isdir(download_location):
             os.mkdir(download_location)
         await df_thumb(m.from_user.id, m.message_id)
@@ -47,7 +47,7 @@ async def save_photo(c,m):
             file_name=download_location
         ) 
         try:
-           await ok.edit_text("✅ Custom thumbnail Saved. \n This thumbnail will be Permanent for all \n future uploads \n Do /deletethumb to clear your thumbnail!")
+           await v.edit_text("✅ Custom thumbnail Saved. \n This thumbnail will be Permanent for all \n future uploads \n Do /deletethumb to clear your thumbnail!")
         except Exception as e:
           log.info(f"#Error {e}")
 
