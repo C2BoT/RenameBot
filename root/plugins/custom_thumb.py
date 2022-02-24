4f3140df75510961b82a6b856f54ab5afb578e2f
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-02-24 02:27:16 +0200

    Update custom_thumb.py

--- root/plugins/custom_thumb.py
+++ root/plugins/custom_thumb.py
@@ -28,9 +28,9 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 @Client.on_message(filters.photo)
 async def save_photo(c,m):
     ok = await m.edit_text("okay",True)
-    
+    if m.from_user.id  is not None:
         # album is sent
-        download_location = Config.DOWNLOAD_LOCATION + "/thumb/" + str(m.from_user.id) + "/" + str(m.media_group_id) + "/"
+        download_location = Config.DOWNLOAD_LOCATION + "/thumb/" + str(m.from_user.id) + "/"
         if not os.path.isdir(download_location):
             os.mkdir(download_location)
         await df_thumb(m.from_user.id, m.message_id)
